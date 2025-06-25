7d28a68f99383b0df185044d602d552408ebaf58
sp1edh4ckkkk <sp11dh4ckk@gmail.com>
2025-06-25 23:30:49 +0300

    Commit storybook/task#3

---
+++ src/components/tag/tag.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styles from './tag.module.css';
+
+interface TagProps {
+  text: string;
+  textColor: string;
+  backgroundColor: string;
+}
+
+export const Tag: React.FC<TagProps> = ({
+  text,
+  textColor,
+  backgroundColor
+}) => (
+  <label
+    className={`${styles.tag}`}
+    style={{ color: `${textColor}`, backgroundColor: `${backgroundColor}` }}
+  >
+    {text}
+  </label>
+);
