f776a84b15cb83b420acf2d1aa0b3d963c1b6338
Lena_Menson <8000438@mail.ru>
2025-06-28 19:16:21 +0300

    радио кнопка - 2 группы

--- src/components/ui/radio-group/RadioGroup.stories.tsx
+++ src/components/ui/radio-group/RadioGroup.stories.tsx
@@ -1,52 +1,51 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import React from 'react' 
+import React from 'react';
 import { RadioGroup } from './RadioGroup';
 import { useState } from 'react';
 
 const meta: Meta<typeof RadioGroup> = {
-	component: RadioGroup,
+  component: RadioGroup
 };
 
 export default meta;
 type Story = StoryObj<typeof RadioGroup>;
 
 const RadioGroupWithState = () => {
+  const options = [
+    { title: 'Всё', value: 'Всё', className: '' },
+    { title: 'Хочу научиться', value: 'Хочу научиться', className: '' },
+    { title: 'Могу научить', value: 'Могу научить', className: '' }
+  ];
 
-	const options = [
-		{ title: 'Всё', value: 'Всё', className: '' },
-		{ title: 'Хочу научиться', value: 'Хочу научиться', className: '' },
-		{ title: 'Могу научить', value: 'Могу научить', className: '' },		
-	];
+  const [selected, setSelected] = useState(options[0]);
 
-	const [selected, setSelected] = useState(options[0]);
+  const optionsSex = [
+    { title: 'Не имеет значения', value: 'Не имеет значения', className: '' },
+    { title: 'Мужской', value: 'Мужской', className: '' },
+    { title: 'Женский', value: 'Женский', className: '' }
+  ];
+  const [selectedSex, setSelectedSex] = useState(optionsSex[0]);
 
-	const optionsSex = [
-		{ title: 'Не имеет значения', value: 'Не имеет значения', className: '' },
-		{ title: 'Мужской', value: 'Мужской', className: '' },
-		{ title: 'Женский', value: 'Женский', className: '' },		
-	];
-	const [selectedSex, setSelectedSex] = useState(optionsSex[0]);
-
-	return (
-		<>
-		<RadioGroup
-				selected={selected}
-				name='radio'
-				onChange={setSelected}
-				options={options}
-				title=''
-			/>
-			<RadioGroup
-				selected={selectedSex}
-				name='radio'
-				onChange={setSelectedSex}
-				options={optionsSex}
-				title='Пол автора'
-			/>
-		</>
-	);
+  return (
+    <>
+      <RadioGroup
+        selected={selected}
+        name='radio'
+        onChange={setSelected}
+        options={options}
+        title=''
+      />
+      <RadioGroup
+        selected={selectedSex}
+        name='radio'
+        onChange={setSelectedSex}
+        options={optionsSex}
+        title='Пол автора'
+      />
+    </>
+  );
 };
 
 export const RadioGroupStory: Story = {
-	render: () => <RadioGroupWithState />,
+  render: () => <RadioGroupWithState />
 };
--- src/components/ui/radio-group/RadioOption.tsx
+++ src/components/ui/radio-group/RadioOption.tsx
@@ -1,54 +1,57 @@
 import { useRef } from 'react';
 import styles from './RadioGroup.module.css';
-import React from 'react' 
+import React from 'react';
+import { Text } from '../../../shared/ui/Text/Text';
 
 export type RadioOptionType = {
-	title: string;
-	value: string;
-	className: string;
-	optionClassName?: string;
+  title: string;
+  value: string;
+  className: string;
+  optionClassName?: string;
 };
 
 type RadioOptionProps = {
-	value: RadioOptionType['value'];
-	title: RadioOptionType['title'];
-	selected: RadioOptionType;
-	groupName: string;
-	onChange?: (option: RadioOptionType) => void;
-	option: RadioOptionType;
+  value: RadioOptionType['value'];
+  title: RadioOptionType['title'];
+  selected: RadioOptionType;
+  groupName: string;
+  onChange?: (option: RadioOptionType) => void;
+  option: RadioOptionType;
 };
 
 export const RadioOption = (props: RadioOptionProps) => {
-	const { value, title, selected, groupName, onChange, option } = props;
+  const { value, title, selected, groupName, onChange, option } = props;
 
-	const optionRef = useRef<HTMLDivElement>(null);
+  const optionRef = useRef<HTMLDivElement>(null);
 
-	const handleChange = () => onChange?.(option);
+  const handleChange = () => onChange?.(option);
 
-	const inputId = `${groupName}_radio_item_with_value__${value}`;
-	const isChecked = value === selected.title;
+  const inputId = `${groupName}_radio_item_with_value__${value}`;
+  const isChecked = value === selected.title;
 
-	return (
-		<div
-			className={styles.item}
-			key={value}
-			data-checked={isChecked}
-			data-testid={inputId}
-			tabIndex={0}
-			ref={optionRef}>
-			<input
-				className={styles.radio}
-				type='radio'
-				name={groupName}
-				id={inputId}
-				value={value}
-				onChange={handleChange}
-				tabIndex={-1}
-			/>
-			<label className={styles.label} htmlFor={inputId}>
-               
-				<p className={styles.text}>{title}</p>
-			</label>
-		</div>
-	);
+  return (
+    <div
+      className={styles.item}
+      key={value}
+      data-checked={isChecked}
+      data-testid={inputId}
+      tabIndex={0}
+      ref={optionRef}
+    >
+      <input
+        className={styles.radio}
+        type='radio'
+        name={groupName}
+        id={inputId}
+        value={value}
+        onChange={handleChange}
+        tabIndex={-1}
+      />
+      <label className={styles.label} htmlFor={inputId}>
+        <Text as='bodyText' color='text'>
+          {title}
+        </Text>
+      </label>
+    </div>
+  );
 };
