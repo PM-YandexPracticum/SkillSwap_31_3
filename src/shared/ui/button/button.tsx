6ef23bc82e8b9a97729bf43c6813b6a720f4caf7
Anastasia Kostyuchenko <165961130+Anastasiya-Kostyuchenko@users.noreply.github.com>
2025-07-01 18:06:20 +0200

    1/button (#35)
    * 1/button
    * Взял компонент на себя так как времени осталось очень мало.
    * Внес исправления и паересобрал компонент
    
    ---------
    
    Co-authored-by: MislanGigisly <homster100@yandex.ru>
    Co-authored-by: MislanGigisly <157415979+MislanGigisly@users.noreply.github.com>

---
+++ src/shared/ui/button/button.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import clsx from 'clsx'; // npm i clsx
+import '../button/button.css';
+
+interface ButtonProps {
+  children: React.ReactNode;
+  onClick: () => void;
+  size?: 'small' | 'medium' | 'large';
+  disabled?: boolean;
+  variant?: 'primary' | 'secondary' | 'tertiary';
+  type?: 'button' | 'submit' | 'reset';
+}
+
+export const Button: React.FC<ButtonProps> = ({
+  children,
+  onClick,
+  size = 'medium',
+  disabled = false,
+  variant = 'primary',
+  type = 'button',
+}) => {
+ const className = clsx('button', variant, size, { disabled });
+
+  return (
+    <button className={className} onClick={onClick} disabled={disabled} type={type}>
+      {children}
+    </button>
+  );
+};
