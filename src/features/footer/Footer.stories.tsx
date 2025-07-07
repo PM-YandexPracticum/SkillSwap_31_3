f0a0a69ff9fdf5e84c6500adf221fb940d36ae51
MislanGigisly <157415979+MislanGigisly@users.noreply.github.com>
2025-07-07 17:41:45 +0300

    собрал главную страницу для масштабирования (#93)

--- src/features/footer/Footer.stories.tsx
+++ src/features/footer/Footer.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
-import { Footer } from './Footer';
+import { Footer } from '@features';
 import './Footer.module.css';
 
 const meta: Meta<typeof Footer> = {
