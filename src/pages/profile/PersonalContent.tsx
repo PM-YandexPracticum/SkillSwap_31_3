cfb8e7693a03cf75cbbae8298c14288ebc219577
sp1edh4ckkkk <sp11dh4ckk@gmail.com>
2025-07-09 01:26:53 +0300

    update

---
+++ src/pages/profile/PersonalContent.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from './profile.module.css';
+
+export const PersonalContent: React.FC = () => (
+  <div className={styles.contentContainer}>
+    <h2>Личные данные</h2>
+    <p>Данные отсутствуют</p>
+  </div>
+);
--- src/shared/ui/SideBar/sideBar.tsx
+++ src/shared/ui/SideBar/sideBar.tsx
@@ -1,19 +1,90 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import RequestsIcon from '../../../images/request.svg';
+import TradesIcon from '../../../images/message-text.svg';
+import FavoritesIcon from '../../../images/like.svg';
+import SkillsIcon from '../../../images/idea.svg';
+import PersonalIcon from '../../../images/user.svg';
+import styles from './sideBar.module.css';
 
+type Tab = 'requests' | 'trades' | 'favorites' | 'skills' | 'personal';
 
-export const SideBar = ({}) => {
-  return (
-    <ul>
-      <li>
-      <Link to='/'>
-        <img
-          // src={ApplicationsIcon}
-          alt='Applications Icon'
-          // className={styles.logo}
-        />
-      </Link>
-      </li>
-    </ul>
-  )
+interface TTabs {
+  activeTab: Tab;
+  onChangeTab: (tab: Tab) => void;
 }
+
+const SideBar: React.FC<TTabs> = ({ activeTab, onChangeTab }) => (
+  <div className={styles.switchesContainer}>
+    <div className={styles.listSwitches}>
+      <button
+        className={
+          activeTab === 'requests'
+            ? `${styles.listSwitchItem} ${styles.activeSwitch}`
+            : styles.listSwitchItem
+        }
+        onClick={() => onChangeTab('requests')}
+      >
+        <img
+          src={RequestsIcon}
+          alt='Requests Icon'
+          className={styles.switchLogo}
+        />
+        <span className={styles.switchSpan}>Заявки</span>
+      </button>
+      <button
+        className={
+          activeTab === 'trades'
+            ? `${styles.listSwitchItem} ${styles.activeSwitch}`
+            : styles.listSwitchItem
+        }
+        onClick={() => onChangeTab('trades')}
+      >
+        <img src={TradesIcon} alt='Trades Icon' className={styles.switchLogo} />
+        <span className={styles.switchSpan}>Мои обмены</span>
+      </button>
+      <button
+        className={
+          activeTab === 'favorites'
+            ? `${styles.listSwitchItem} ${styles.activeSwitch}`
+            : styles.listSwitchItem
+        }
+        onClick={() => onChangeTab('favorites')}
+      >
+        <img
+          src={FavoritesIcon}
+          alt='Favorites Icon'
+          className={styles.switchLogo}
+        />
+        <span className={styles.switchSpan}>Избранное</span>
+      </button>
+      <button
+        className={
+          activeTab === 'skills'
+            ? `${styles.listSwitchItem} ${styles.activeSwitch}`
+            : styles.listSwitchItem
+        }
+        onClick={() => onChangeTab('skills')}
+      >
+        <img src={SkillsIcon} alt='Skills Icon' className={styles.switchLogo} />
+        <span className={styles.switchSpan}>Мои навыки</span>
+      </button>
+      <button
+        className={
+          activeTab === 'personal'
+            ? `${styles.listSwitchItem} ${styles.activeSwitch}`
+            : styles.listSwitchItem
+        }
+        onClick={() => onChangeTab('personal')}
+      >
+        <img
+          src={PersonalIcon}
+          alt='Personal Icon'
+          className={styles.switchLogo}
+        />
+        <span className={styles.switchSpan}>Личные данные</span>
+      </button>
+    </div>
+  </div>
+);
+
+export default SideBar;
