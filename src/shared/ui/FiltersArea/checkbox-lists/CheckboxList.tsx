c95f5946736a6ce1d30c3aa62152f122757942d0
Lena_Menson <8000438@mail.ru>
2025-07-09 23:57:39 +0300

    задача 101 Fix_чекбоксы в списках (сторибук)_1

---
+++ src/shared/ui/FiltersArea/checkbox-lists/CheckboxList.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import styles from './CheckboxList.module.css';
+import { CheckboxOption } from './CheckboxOption';
+import type { CheckboxListOptionType } from './CheckboxOption';
+import { useState } from 'react';
+import clsx from 'clsx';
+
+type CheckboxListProps = {
+  name: string;
+  options: CheckboxListOptionType[];
+  selected: string[];
+  onChange?: (value: string) => void;
+  isSubcategory?: boolean;
+  checkboxClass?: string;
+  onClick?: () => void;
+};
+
+export const CheckboxList = (props: CheckboxListProps) => {
+  const { name, options, selected, onChange, checkboxClass, onClick } =
+    props;
+
+  const [selectedOptions, setselectedOptions] = useState<string[]>([]);
+
+  const handleChange = (value: string) => {
+    setselectedOptions((prev) =>
+      prev.includes(value) ? prev.filter((s) => s !== value) : [...prev, value]
+    );
+    return onChange?.(value);
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.group}>
+        {options.map((option) => (
+          <CheckboxOption
+            key={option.value}
+            groupName={name}
+            value={option.value}
+            selected={selectedOptions}
+            onChange={() => handleChange(option.value)}
+            option={option}
+            checkboxClass={clsx(styles.checkboxCategory)}
+            onClick={onClick}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
