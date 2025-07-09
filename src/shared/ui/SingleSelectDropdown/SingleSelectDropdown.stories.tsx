a71553d4ec2da8f16b05055209b1ca3a78a8b9b5
Alex-Uvarov <121911179+Alex-Uvarov@users.noreply.github.com>
2025-07-09 23:57:23 +0300

    Исправлен дропдаун с единичным выбором (#73)
    * Исправлен дропдаун с единичным выбором
    
    * Добавлена возможность задать предустановленное значение
    
    * Исправления

---
+++ src/shared/ui/SingleSelectDropdown/SingleSelectDropdown.stories.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Meta, StoryObj } from '@storybook/react';
+
+import { SingleSelectDropdown } from './SingleSelectDropdown';
+
+const meta: Meta = {
+  title: 'SingleSelectDropdown',
+  component: SingleSelectDropdown,
+  tags: ['autodocs']
+};
+
+export default meta;
+
+type Story = StoryObj<typeof SingleSelectDropdown>;
+
+export const Default: Story = {
+  args: {
+    values: [
+      'Не указан',
+      'Мужской',
+      'Женский'
+    ],
+    placeholder: 'Не указан',
+    label: 'Пол',
+  },
+  decorators: [
+    (Story) => (
+      <div style={{ width: '436px' }}>
+        <Story />
+      </div>
+    )
+  ]
+};
